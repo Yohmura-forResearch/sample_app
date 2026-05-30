94c2abc8db36ab1669f557fb8dd1693e1c25bd2a
Yohmura-forResearch <z26017yo@aiit.ac.jp>
2026-05-30 17:44:33 +0000

    Finish ch09

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -18,10 +18,7 @@ module SessionsHelper
   # 記憶トークンのcookieに対応するユーザーを返す
   def current_user
     if (user_id = session[:user_id])
-      user = User.find_by(id: user_id)
-      if user && session[:session_token] == user.session_token
-        @current_user = user
-      end
+      @current_user ||= User.find_by(id: user_id)
     elsif (user_id = cookies.encrypted[:user_id])
       user = User.find_by(id: user_id)
       if user && user.authenticated?(cookies[:remember_token])
@@ -36,20 +33,21 @@ module SessionsHelper
     user && user == current_user
   end
 
-  # 現在ログイン中のユーザーを返す（いる場合）
-  def current_user
-    if session[:user_id]
-      @current_user ||= User.find_by(id: session[:user_id])
-    end
-  end
-
   # ユーザーがログインしていればtrue、その他ならfalseを返す
   def logged_in?
     !current_user.nil?
   end
 
+  # 永続的セッションを破棄する
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
+
     # 現在のユーザーをログアウトする
   def log_out
+    forget(current_user)
     reset_session
     @current_user = nil   # 安全のため
   end
